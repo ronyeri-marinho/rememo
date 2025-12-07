bc3b32a322475fc7b4a952474753839082f398c7
Ronyeri Marinho <76921589+ronyeri-marinho@users.noreply.github.com>
2025-12-07 18:41:15 -0300

    🎉 init: initial project setup

---
+++ src/main/java/br/com/rememo/RememoApplication.java
@@ -0,0 +1,13 @@
+package br.com.rememo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RememoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RememoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rememo/RememoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rememo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RememoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
