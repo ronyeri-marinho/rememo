18fa0bac78fcf44c784d757fbe7b1a74c590e10c
ronyeri-marinho <ronyerimarinho19@gmail.com>
2026-01-24 14:49:52 -0300

    feat: configuring security properties

---
+++ src/main/java/br/com/rememo/application/infrastructure/configuration/properties/SecurityConfigurationProperties.java
@@ -0,0 +1,23 @@
+package br.com.rememo.application.infrastructure.configuration.properties;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+@ConfigurationProperties("rememo.security")
+public class SecurityConfigurationProperties {
+
+    @Value("#{'${rememo.security.allowed-origins}'.split(',')}")
+    private List<String> allowedOrigins;
+
+    public List<String> getAllowedOrigins() {
+        return allowedOrigins;
+    }
+
+    public void setAllowedOrigins(List<String> allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
+}
